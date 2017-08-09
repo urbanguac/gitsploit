c890bb91a5f9d44f7ed0d91a46a926e2a1b84545
pshah123 <priansh123@me.com>
2017-08-09 13:26:30 -0400

    options

--- a_million_commits.sh
+++ a_million_commits.sh
@@ -1,3 +1,4 @@
+NUMBER_OF_COMMITS_PER_DAY = 50
 for Y in {2016..2018}
 do
 	mkdir $Y
@@ -11,13 +12,21 @@ do
 			cd $Y/$M
 			mkdir $D
 			cd $D
-			echo "Committed on $M/$D/$Y" > commit.md
-			cd ../../../
-			export GIT_COMMITTER_DATE="$Y-$M-$D 12:00:00"
-			export GIT_AUTHOR_DATE="$Y-$M-$D 12:00:00"
-			git add $Y/$M/$D/commit.md -f
-			git commit --date="$Y-$M-$D 12:00:00" -m "Committed on $M $D $Y"
+			for i in {1..$NUMBER_OF_COMMITS_PER_DAY}
+			do
+				cd $Y/$M/$D
+				echo "Commit # $i on $M/$D/$Y" > commit$i.md
+				cd ../../../
+				export GIT_COMMITTER_DATE="$Y-$M-$D 12:00:00"
+				export GIT_AUTHOR_DATE="$Y-$M-$D 12:00:00"
+				git add $Y/$M/$D/commit$i.md -f
+				git commit --date="$Y-$M-$D 12:00:00" -m "Committed $i on $M $D $Y"
+			done
 		done
 	done
 done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git add --all && git commit -m "cleanup"
 git push origin master
