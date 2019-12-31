cd93085a281f45d745cd29510f892319a6af5849
Priansh Shah <shah@priansh.com>
2019-12-31 12:00:00 -0500

    cleanup

--- a_million_commits.sh
+++ a_million_commits.sh
@@ -1,5 +1,5 @@
 # a million commits
-for Y in {2016..2018}
+for Y in {2019..2019}
 do
 	mkdir $Y
 	cd $Y
@@ -7,19 +7,20 @@ do
 	do
 		mkdir $M
 		cd $M
-		for D in {1,6,12,13,17,21,22,23,27,30}
+		for D in {1..31}
 		do
 			mkdir $D
 			cd $D
+            iter=$(shuf -i 5-15 -n 1)
 			# the next line controls number of commits per day
-	#		for i in {1..10}
-	#		do
-			echo "Commit $i on $M/$D/$Y" > commit.md
-			export GIT_COMMITTER_DATE="$Y-$M-$D 12:00:00"
-			export GIT_AUTHOR_DATE="$Y-$M-$D 12:00:00"
-			git add commit.md -f
-			git commit --date="$Y-$M-$D 12:00:00" -m "Committed $i on $M $D $Y"
-	#		done
+            for ((i=0; i<$iter; i++))
+			do
+    			echo "Commit $i on $M/$D/$Y" > commit.md
+			    export GIT_COMMITTER_DATE="$Y-$M-$D 12:00:00"
+			    export GIT_AUTHOR_DATE="$Y-$M-$D 12:00:00"
+		    	git add commit.md -f
+	    		git commit --date="$Y-$M-$D 12:00:00" -m "Committed $i on $M $D $Y"
+			done
 			cd ../
 		done
 		cd ../
